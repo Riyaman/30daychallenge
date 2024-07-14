b43845ab75b26f5fa6cebeec3edd1099a3f861ac
Janhavi Ganorkar <janhaviganorkar12@gmail.com>
2024-07-14 08:05:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+// Actcivity 1
+// task 1
+let num1 = 76
+let num2 = 65
+let sum = num1 + num2
+console.log(sum)
+// Task 2
+let a = 23
+let b = 10
+let subs = a - b
+console.log(subs)
+// Task 3
+let f = 23
+let h = 4
+let mult = f * h
+console.log(mult)
+// Task 4
+let k = 23
+let d = 4
+let divi  = k / d
+console.log(divi)
+// Task 5
+let y = 23
+let u = 4
+let mod  = y % u
+console.log(mod)
+
+// Activity 2
+// Task 6
+let cf = 76;
+cf += 8;
+console.log("cf is:"); 
+console.log(cf)
+// Task 7
+let kh = 76;
+kh -= 8;
+console.log("kh is:"); 
+console.log(kh)
+// Activity 3
+// Task 8
+let ns = 3
+let bx = 6
+if (ns > bx){
+    console.log(ns)
+}
+else if (ns < bx) {
+    console.log(bx)
+}
+else {
+    console.log("Invalid")
+}
+// Task 9
+let ms = 65
+if (ms >=6){
+    console.log("number is greater")
+}
+else if (ms <=6){
+    console.log("number is smaller")
+}
+// Task 10
+vs = 63
+sd = 61
+if (vs == sd){
+    console.log(vs)
+}
+
+
+if (vs === sd){
+    console.log("number is exat same")
+}
+// Activity 4
+// Task 11
+let ss = 44
+let mm = 22
+let add = ss + mm
+console.log(add)
+ if (ss > mm && add ===66){
+    console.log("Conditon satisfied")
+ }
+ else if (ss>mm || add ===66){   //Task 12
+    console.log("number is greater but additon not same")
+ }
+//  Acitivity 5
+// Task 14
+let ga = 53
+let hd = ga > 50 ? "ok" : "noh"
+console.log(hd)
